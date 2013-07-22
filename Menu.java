03069edc2dd8db1ef410549f93ce97cdf5f0db42
Kimler Corey <kimler@gmail.com>
2013-07-22 17:45:04 -0400

    Update Menu.java
    Somehow I had the old version of another file here. Thanks for pointing that out Joe!

--- Menu.java
+++ Menu.java
@@ -1,70 +1,140 @@
+/*
+ * Author: Kimler Corey
+ * CMSC 412
+ * 
+ */
 package simulator;
 
-/**
-*
-* @author kimler (kc) Corey
-*/
+import java.util.Random;
+import java.util.Scanner;
 
-public class Simulator {
-    
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String[] args) {
-        
-        //init area
-        
-        // Selected menu item
-        int sel;
-        //new menu and user input
-        Menu menu = new Menu();
-        
+public class Menu {
+
+    private int selected; //stores the menu selection
+    private boolean showReferenceString;
+    public String selectedReferenceStringBuffer;
+    static final String VFRAMES = "01234567";
+    static Random rnd = new Random();
+
+    // does the whole menu logic (presentation, validation and returns reult)
+    public int doMenu() {
+
+        this.showMenu();
+        int token;
+
+
         do {
-            sel = menu.doMenu();
-            
-            switch(sel)
-            {
-                case 0: // 0 – Exit
-                System.out.println("bye!!");
-                break;
-                
-                case 1: // 1 – Read reference string
-                menu.readRS();
-                break;
-                
-                case 2: // 2 – Generate reference string
-                menu.generateRS();
-                break;
-                
-                case 3: // 3 – Display current reference string
-                if (menu.selectedReferenceStringBuffer==null) {menu.block(1); break;}
-                menu.toggleDisplayRS();
-                break;
-                
-                case 4: // 4 – Simulate FIFO
-                if (menu.selectedReferenceStringBuffer==null) {menu.block(2); break;}
-                Pager fifo = new Pager (menu.selectedReferenceStringBuffer,0);
-                fifo.run();
-                break;
-                
-                case 5: // 5 – Simulate OPT
-                if (menu.selectedReferenceStringBuffer==null) {menu.block(2); break;}
-                Pager opt = new Pager (menu.selectedReferenceStringBuffer,1);
-                opt.run();
-                break;
-                
-                case 6: // 6 – Simulate LFU
-                if (menu.selectedReferenceStringBuffer==null) {menu.block(2); break;}
-                Pager lfu = new Pager (menu.selectedReferenceStringBuffer,2);
-                lfu.run();
-                break;
-                
-                default: System.out.println("Wow! no clue how anyone could every get here.");
+            Scanner scanner = new Scanner(System.in);
+
+            // non integers shouldn't be accespted
+            while (!scanner.hasNextInt()) {
+                System.out.print("\nValid selections are 0-6 (try again)> ");
+                scanner.nextLine();
             }
-            System.out.println("-----------------------------------------------");
-        } //end do
-        while (sel != 0);
-        
-        //String menu = "menu:\n";   
+
+            token = scanner.nextInt();
+
+            //other integers shouldn't work
+            if (token > 6 || token < 0) {
+                System.out.print("\nSeriously? you can choose either 0,1,2,3,4,5 or 6. Everything else just ain't gonna work. (try again)\n> ");
+            }
+
+        } while (token > 6 || token < 0);
+
+        this.selected = token;
+
+        // return the valid selection to caller
+        return this.selected;
+    }
+
+    //menu view
+    public void showMenu() {
+
+        String menu = "Memory Paging Simulation Menu:\n\n";
+
+        // display the reference string if display is true
+        if (this.showReferenceString) {
+            menu += "[Current RS:" + this.selectedReferenceStringBuffer + "]\n\n";
+        }
+
+        menu += "0 – Exit\n";
+        menu += "1 – Read reference string\n";
+        menu += "2 – Generate reference string\n";
+        menu += "3 – Toggle Display of current reference string\n";
+        menu += "4 – Simulate FIFO\n";
+        menu += "5 – Simulate OPT\n";
+        menu += "6 – Simulate LFU\n> ";
+
+        System.out.print(menu);
+    }
+
+    // reads in a new reference string from the user
+    public void readRS() {
+
+        System.out.println("Input the characters: ");
+        Scanner input = new Scanner(System.in);
+        //validates the input uses only digits 0-7
+        String intext = input.next();
+        if (intext.matches("^[0-7]+$")) {
+            this.selectedReferenceStringBuffer = intext;
+        } else {
+            System.out.println("Please input only numbers between 0 and 7 becasue thats how many virtual frames there are.");
+        }
+        //this.selectedReferenceStringBuffer
+    }
+
+    // generates a new reference string for the user
+    public void generateRS() {
+        int numberofframes = 0;
+
+        System.out.println("Enter how many frames to process (0 for default string):");
+
+        Scanner scan = new Scanner(System.in);
+        try {
+            numberofframes = scan.nextInt();
+        } catch (Exception e) {
+            System.out.println("So wrong.");
+        }
+
+        if (numberofframes == 0) {
+            this.selectedReferenceStringBuffer = "30620373207423723";
+        } else if (numberofframes > 0) {
+            this.selectedReferenceStringBuffer = getRandomString(numberofframes);
+        } else {
+            System.out.println("Just seems wrong.");
+        }
+
+    }
+
+    // attempts to toggle the display for reference string
+    public void toggleDisplayRS() {
+        System.out.println("Display current reference string");
+        if (this.showReferenceString == true) {
+            this.showReferenceString = false;
+        } else {
+            this.showReferenceString = true;
+        }
+
+
+    }
+
+    //blocking messages for user menu actions
+    public void block(int errMessage) {
+
+        if (errMessage == 1) {
+            System.out.println("You can't view the reference string until you have one!");
+        } else {
+            System.out.println("Please create a reference string before doing that.");
+        }
+    }
+
+    //generates a random string or returns the default string 
+    public String getRandomString(int len) {
+
+        StringBuilder gen = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            gen.append(VFRAMES.charAt(rnd.nextInt(VFRAMES.length())));
+        }
+        return gen.toString();
     }
 }
